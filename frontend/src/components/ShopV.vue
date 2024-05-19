2f2bb877aa7706aabcf1d00c8e5c0128040b033e
DucBoB <hoaiduc0210@gmail.com>
2024-05-20 01:49:10 +0700

    duc up font

---
+++ frontend/src/components/ShopV.vue
@@ -0,0 +1,154 @@
+<template>
+    <section class="shop_section layout_padding">
+    <div class="container">
+      <div class="d-flex justify-content-center">
+        <h2 class="heading_style">
+          Our Shop
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="d-flex align-items-center h-100">
+            <div class="shop-detail">
+              <h5>
+                Everything You Need
+              </h5>
+              <p>
+                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="shop_img-box">
+            <img src="images/gift.jpg" alt="" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end shop section -->
+
+
+
+
+
+  <!-- info section -->
+  <section class="info_section ">
+    <div class="container ">
+      <div class="row  mb-3 pb-4">
+        <div class="col-md-3 info_logo">
+          <div class="logo-box">
+            <img src="images/logo-white.png" alt="">
+            <span>
+              Transportz
+            </span>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sedconsectetur
+          </p>
+          <div class="info_social">
+            <div>
+              <a href="">
+                <img src="images/fb.png" alt="">
+              </a>
+            </div>
+            <div>
+              <a href="">
+                <img src="images/twitter.png" alt="">
+              </a>
+            </div>
+            <div>
+              <a href="">
+                <img src="images/g-plus.png" alt="">
+              </a>
+            </div>
+            <div>
+              <a href="">
+                <img src="images/linkedin.png" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 info_address">
+          <h5>
+            Address
+          </h5>
+          <p>
+            Healing Center, 176 W Street name,
+            New York, NY 10014, US
+          </p>
+          <p>
+            (+71) 8522369417
+          </p>
+          <p>
+            (+71) 8522369417
+          </p>
+          <p>
+            <a href="">
+              transportz@gmail.com
+            </a>
+          </p>
+        </div>
+        <div class="col-md-3 info_links">
+
+          <div class="info_nav ">
+            <nav class="">
+              <ul>
+                <h5>
+                  Links
+                </h5>
+                <li>
+                  <a href="index.html"> Home</a>
+                </li>
+                <li>
+                  <a href="about.html">About</a>
+                </li>
+                <li>
+                  <a href="service.html"> Service</a>
+                </li>
+                <li>
+                  <a href="shop.html"> Shop</a>
+                </li>
+                <li>
+                  <a href="company.html"> Company</a>
+                </li>
+                <li>
+                  <a href="contact.html">Contact us</a>
+                </li>
+
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="col-md-3 info_news">
+          <h5>
+            Newsletter
+          </h5>
+          <form action="">
+            <div>
+              <input type="text" placeholder="Your Name">
+            </div>
+            <div>
+              <input type="email" placeholder="Email">
+            </div>
+            <div class="d-flex justify-content-end">
+              <button type="submit">
+                Subscribe
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+
+    </div>
+  </section>
+</template>
+
+<style scoped>
+@import url("@/assets/css/responsive.css");
+@import url("@/assets/css/bootstrap.css");
+@import url("@/assets/css/style.css");
+</style>
